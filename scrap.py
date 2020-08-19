2787bdfea6dbf8b57279a277fba0d0cc5064f58f
Abdul Syed <dev.abdul01@gmail.com>
2020-08-19 05:37:15 -0400

    parsed over html content to extract needed html info

--- scrap.py
+++ scrap.py
@@ -3,28 +3,33 @@ from bs4 import BeautifulSoup
 from time import sleep
 
 
-base_url = "http://books.toscrape.com/"
+base_url = "http://books.toscrape.com/catalogue/page-{}.html"
 
 
 all_books = []
 
-response = requests.get(base_url)
-soup = BeautifulSoup(response.text, 'html.parser')
+for n in range(1,51):
+    scrap_url = base_url.format(n)
+    response = requests.get(scrap_url)
 
+    soup = BeautifulSoup(response.text, "html.parser")
 
-books = soup.find_all(class_ = "product_pod")
+    books = soup.find_all(class_ = "product_pod")
 
-for book in books:
-    
-    all_books.append({
-        'title': "asasassasas",
-        "price": book.find(class_ = "price_color").get_text(),
-        "availability": book.find(class_ = "instock").get_text()
+    for book in books:
 
-    }
-    )
+        all_books.append({
+            # 'title': book.find('img', alt = True),
+            'title': book.find('img')['alt'],
+            "price": book.find(class_ = "price_color").get_text(),
+            "availability": book.find(class_ = "instock").get_text()
 
+        }
+        )
+
+ 
 
 
 print(all_books)
+print(len(all_books))
 # print(soup.body)
