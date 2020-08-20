b57a67066714cac3e6694642168f25a4677cde73
Abdul Syed <dev.abdul01@gmail.com>
2020-08-20 18:51:26 -0400

    scrapping complete

--- scrap.py
+++ scrap.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 from time import sleep
+import boto3
+import json
 
 
 base_url = "http://books.toscrape.com/catalogue/page-{}.html"
@@ -8,28 +10,48 @@ base_url = "http://books.toscrape.com/catalogue/page-{}.html"
 
 all_books = []
 
-for n in range(1,51):
-    scrap_url = base_url.format(n)
-    response = requests.get(scrap_url)
+def scrap_books():
 
-    soup = BeautifulSoup(response.text, "html.parser")
+    for n in range(1,51):
+        scrap_url = base_url.format(n)
+        print(f"scrapping page {n} of website")
+        response = requests.get(scrap_url)
 
-    books = soup.find_all(class_ = "product_pod")
 
-    for book in books:
+        soup = BeautifulSoup(response.text, "html.parser")
 
-        all_books.append({
-            # 'title': book.find('img', alt = True),
-            'title': book.find('img')['alt'],
-            "price": book.find(class_ = "price_color").get_text(),
-            "availability": book.find(class_ = "instock").get_text()
 
-        }
-        )
+        books = soup.find_all(class_ = "product_pod")
+
+        for book in books:
+
+            all_books.append({
+                # 'title': book.find('img', alt = True),
+                'title': book.find('img')['alt'],
+                "price": book.find(class_ = "price_color").get_text(),
+                "availability": book.find(class_ = "instock").get_text()
+
+            }
+            )
+
+        # sleep(1)
+    return all_books
 
  
 
 
-print(all_books)
-print(len(all_books))
 # print(soup.body)
+
+
+books = scrap_books()
+print(len(all_books))
+
+print(books[1])
+
+def save(books):
+    with open('app.json', "w") as fp:
+        json.dump(books, fp)
+
+save(books)
+
+
